dda8e55cebfcbf1a74fc5d543ac2ea07c7c3fae4
Amna Khan <akhan145542@me.bergen.edu>
2023-05-30 17:17:55 -0400

    completed day 1

--- day 1/helloWorld.py
+++ day 1/helloWorld.py
@@ -1,5 +1,10 @@
 # Output & Input: print, input
 
 # print Hello World!
-
+print ("Hello World!")
 # print Hello {user}! using input
+name= input ("Enter your name:")
+print ("Hello " + names)
+num = 1
+print ("It's 0!")
+if num ==
