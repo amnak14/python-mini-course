d57148378e6767e78cc697784e37bf09949c6d98
Amna Khan <akhan145542@me.bergen.edu>
2023-06-01 17:09:36 -0400

    Completed Day 2

--- day 1/if_else_solution.py
+++ day 1/if_else_solution.py
@@ -11,21 +11,31 @@
 #     print("It's false")
 
 
-num = 1
+num = 4
 print("Num is ", num)
 if num == 0:
     print("It's 0!")
-if num == 1:
+elif num == 1:
     print("It's 1!")
-
+else:
+    print("I don't know what num is but it's not 1 or 0")
 # and
 
 # or
-
+if True and False:
+    print()
 # not
 
+if True or False:
 # comparision == !=
 
 # operators += -= *= /= %= //= **=
 
 # switch (match) statements
+
+
+
+color1= "red"
+color2= "pink"
+if color1=="red" or color=="pink":
+    print("It's reddish")
